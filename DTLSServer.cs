183252154d7ffee4d7fbb5b73597dff1f99535f3
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-07-11 20:44:46 +0530

    © RISHIKESHAN L

--- DTLSServer.cs
+++ DTLSServer.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright [2019] RISHIKESHAN LAVAKUMAR
+﻿/* Copyright [2019] RISHIKESHAN LAVAKUMAR <github-public [at] ris.fi>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
