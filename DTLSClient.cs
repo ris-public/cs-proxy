16e731632e4daf26219bcb8df2b5d11e4b876db2
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-10-01 05:12:43 +0530

    v1.1.1.

--- DTLSClient.cs
+++ DTLSClient.cs
@@ -4,14 +4,14 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 
 
 using System;
@@ -24,7 +24,7 @@ using System.Buffers;
 using Rishi.PairStream;
 
 namespace Rishi{
-	public class DTLSClient{
+		public class DTLSClient{
 
 				///<summary>
 				///Verbosity.
@@ -62,53 +62,53 @@ namespace Rishi{
 				///The <see cref="System.IO.Stream" />.
 				///</summary>
 				protected Stream S;
-		protected byte[] PSK;
+				protected byte[] PSK;
 
-		public DTLSClient(string hostname, string port, byte[] PSK){
-			this.Proc = new Process();
-			this.port=port;
-			this.hostname=hostname;
-			this.PSK=PSK;
-		}
-		public void Start(){
+				public DTLSClient(string hostname, string port, byte[] PSK){
+						this.Proc = new Process();
+						this.port=port;
+						this.hostname=hostname;
+						this.PSK=PSK;
+				}
+				public void Start(){
 
-			this.Proc.StartInfo.FileName=$"{Unbuffer}";
-			this.Proc.StartInfo.UseShellExecute = false;
-			this.Proc.StartInfo.RedirectStandardOutput = true;
-			string psk_hex=BitConverter.ToString(PSK).Replace("-", String.Empty);
-			//Proc.StartInfo.Arguments=$"{Unbuffer_Args} openssl s_server -nocert -dtls -accept {port} -psk {psk_hex}";
-			string PrCommand=$"openssl";
-			string ClArguments=$"s_client -dtls -connect {hostname}:{port} -psk {psk_hex} -quiet";
-			SetColour(5,0);
-			System.Console.Error.WriteLine(Proc.StartInfo.FileName + " " + Proc.StartInfo.Arguments);
-			ResetColour();
-			//Proc.OutputDataReceived += ret2;
-			Proc.StartInfo.RedirectStandardInput=true;
-			Proc.Start();
+						this.Proc.StartInfo.FileName=$"{Unbuffer}";
+						this.Proc.StartInfo.UseShellExecute = false;
+						this.Proc.StartInfo.RedirectStandardOutput = true;
+						string psk_hex=BitConverter.ToString(PSK).Replace("-", String.Empty);
+						//Proc.StartInfo.Arguments=$"{Unbuffer_Args} openssl s_server -nocert -dtls -accept {port} -psk {psk_hex}";
+						string PrCommand=$"openssl";
+						string ClArguments=$"s_client -dtls -connect {hostname}:{port} -psk {psk_hex} -quiet";
+						SetColour(5,0);
+						System.Console.Error.WriteLine(Proc.StartInfo.FileName + " " + Proc.StartInfo.Arguments);
+						ResetColour();
+						//Proc.OutputDataReceived += ret2;
+						Proc.StartInfo.RedirectStandardInput=true;
+						Proc.Start();
 
-			A = Proc.StandardInput;
-			B = Proc.StandardOutput;
-		}
+						A = Proc.StandardInput;
+						B = Proc.StandardOutput;
+				}
 
-		public Stream GetStream(){
-			return new pair(B,A);
-		}
-		public void Kill(){
-			Proc.Kill();
-		}
-		public void Close(){
-			Proc.Close();
-		}
-		public void WaitForExit(){
-		}
+				public Stream GetStream(){
+						return new pair(B,A);
+				}
+				public void Kill(){
+						Proc.Kill();
+				}
+				public void Close(){
+						Proc.Close();
+				}
+				public void WaitForExit(){
+				}
 
-		private static void SetColour(int fg, int bg){
-			System.Console.Error.WriteLine($"\u001b[1;3{fg}m");
-			System.Console.Error.WriteLine($"\u001b[4{bg}m");
+				private static void SetColour(int fg, int bg){
+						System.Console.Error.WriteLine($"\u001b[1;3{fg}m");
+						System.Console.Error.WriteLine($"\u001b[4{bg}m");
+				}
+				private static void ResetColour(){
+						System.Console.Error.WriteLine("\u001b[39m");
+						System.Console.Error.WriteLine("\u001b[49m");
+				}
 		}
-		private static void ResetColour(){
-			System.Console.Error.WriteLine("\u001b[39m");
-			System.Console.Error.WriteLine("\u001b[49m");
-		}
-	}
 }
